ba200a5e28aa5566a641959d5797e978abb4dc0b
Joaquin Gonzalez <jpgonzalezra@gmail.com>
2024-05-24 17:41:48 -0300

    feat: add minting functionality to InterestBearingToken

--- src/InterestBearingToken.sol
+++ src/InterestBearingToken.sol
@@ -5,9 +5,51 @@ import { ERC20 } from "solmate/tokens/ERC20.sol";
 import { Owned } from "solmate/auth/Owned.sol";
 
 contract InterestBearingToken is ERC20, Owned {
+    /* ============ Structs ============ */
+    // nothing for now
+
+    /* ============ Errors ============ */
+    error InvalidRecipient(address recipient_);
+    error InsufficientAmount(uint256 amount_);
+
+    /* ============ Variables ============ */
     uint256 public interestRate; // interest rate in BPS
 
+    /* ============ Modifiers ============ */
+    // nothing for now
+
+    /* ============ Constructor ============ */
     constructor(uint256 interestRate_) ERC20("IBToken", "IB", 6) Owned(msg.sender) {
         interestRate = interestRate_;
+    }
+
+    /* ============ Interactive Functions ============ */
+    function mint(address to_, uint256 amount_) external onlyOwner {
+        _revertIfInvalidRecipient(to_);
+        _revertIfInsufficientAmount(amount_);
+        // _updateInterest(to_)
+        _mint(to_, amount_);
+    }
+
+    /* ============ Internal Interactive Functions ============ */
+
+    function _updateInterest(address to_) internal {
+        // nothing for now
+    }
+
+    /**
+     * @dev   Reverts if the amount of a `mint` or `burn` is equal to 0.
+     * @param amount_ Amount to check.
+     */
+    function _revertIfInsufficientAmount(uint256 amount_) internal pure {
+        if (amount_ == 0) revert InsufficientAmount(amount_);
+    }
+
+    /**
+     * @dev   Reverts if the recipient of a `mint` or `transfer` is address(0).
+     * @param recipient_ Address of the recipient to check.
+     */
+    function _revertIfInvalidRecipient(address recipient_) internal pure {
+        if (recipient_ == address(0)) revert InvalidRecipient(recipient_);
     }
 }
--- test/InterestBearingToken.t.sol
+++ test/InterestBearingToken.t.sol
@@ -16,14 +16,58 @@ contract InterestBearingTokenTest is Test {
     address bob = address(3);
     address charly = address(4);
 
-    function setUp() public {
+    error InvalidRecipient(address recipient_);
+    error InsufficientAmount(uint256 amount_);
+
+    function setUp() external {
         vm.prank(owner);
         token = new InterestBearingToken(1e3); // 10% APY in BPS
     }
 
-    function testInitialization() public view {
+    function testInitialization() external view {
+        assertEq(token.owner(), owner);
         assertEq(token.name(), "IBToken");
         assertEq(token.symbol(), "IB");
         assertEq(token.interestRate(), 1e3);
+    }
+
+    function testMintingByOwner() external {
+        vm.prank(owner);
+        token.mint(alice, 1000 * 10e6); // Mint 1000 tokens to alice
+        assertEq(token.balanceOf(alice), 1000 * 10e6);
+    }
+
+    function testMintingByNonOwner() external {
+        // Alice tries to mint tokens
+        vm.prank(alice);
+        vm.expectRevert("UNAUTHORIZED");
+        token.mint(alice, 1000 * 10e6);
+    }
+
+    function testMintingByOwnerAfterOwnershipTransfer() external {
+        vm.prank(owner);
+        // Transfer ownership to alice
+        token.transferOwnership(alice);
+
+        // owner should not be able to mint tokens anymore
+        vm.expectRevert("UNAUTHORIZED");
+        token.mint(bob, 1000 * 10e6);
+
+        // alice should be able to mint tokens now
+        vm.prank(alice);
+        token.mint(bob, 1000 * 10e6);
+        assertEq(token.balanceOf(bob), 1000 * 10e6);
+    }
+
+    function testMintingInvalidRecipient() external {
+        vm.prank(owner);
+        vm.expectRevert(abi.encodeWithSelector(InvalidRecipient.selector, address(0)));
+        token.mint(address(0), 1000 * 10e6);
+    }
+
+    function testMintingInsufficientAmount() external {
+        vm.prank(owner);
+        vm.expectRevert(abi.encodeWithSelector(InsufficientAmount.selector, 0));
+        token.mint(alice, 0);
     }
 }
