0250d073d5a75fa71f5710e1019b461534afa7bf
Joaquin Gonzalez <jpgonzalezra@gmail.com>
2024-05-27 14:06:58 -0300

    refactor: Update scale factor constant in InterestBearingToken

--- src/InterestBearingToken.sol
+++ src/InterestBearingToken.sol
@@ -13,14 +13,17 @@ import { IInterestBearingToken } from "./intefaces/IInterestBearingToken.sol";
 contract InterestBearingToken is IInterestBearingToken, ERC20Extended, Owned {
     /* ============ Variables ============ */
 
+    /// @notice Scale factor used to convert basis points (bps) into decimal fractions.
+    uint16 internal constant SCALE_FACTOR = 10_000; // Ex, 100 bps (1%) is converted to 0.01 by dividing by 10,000
+
     /// @notice The number of seconds in a year.
     uint32 internal constant SECONDS_PER_YEAR = 31_536_000;
 
-    /// @notice The minimum yearly rate of interest in basis points (BPS).
+    /// @notice The minimum yearly rate of interest in basis points (bps).
     uint16 public constant MIN_YEARLY_RATE = 100; // This represents a 1% annual percentage yield (APY).
 
-    /// @notice The maximum yearly rate of interest in basis points (BPS).
-    uint16 public constant MAX_YEARLY_RATE = 4000; // This represents a 40% annual percentage yield (APY).
+    /// @notice The maximum yearly rate of interest in basis points (bps).
+    uint16 public constant MAX_YEARLY_RATE = 4_000; // This represents a 40% annual percentage yield (APY).
 
     uint256 internal _totalSupply;
     uint16 public yearlyRate;
@@ -194,7 +197,7 @@ contract InterestBearingToken is IInterestBearingToken, ERC20Extended, Owned {
         unchecked {
             timeElapsed = block.timestamp - _lastUpdateTimestamp[account_];
         }
-        return (_balances[account_] * timeElapsed * yearlyRate) / (10_000 * uint256(SECONDS_PER_YEAR));
+        return (_balances[account_] * timeElapsed * yearlyRate) / (SCALE_FACTOR * uint256(SECONDS_PER_YEAR));
     }
 
     /**
